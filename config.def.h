9c5053bd2a66194d51be4c1f7e7e46fd615071de
pankx <a3427173515@163.com>
2023-05-14 15:27:40 +0800

    终端右键粘贴

--- config.def.h
+++ config.def.h
@@ -176,6 +176,7 @@ static uint forcemousemod = ShiftMask;
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
+	{ XK_ANY_MOD,           Button3, clippaste,      {.i =  0} },
 	{ XK_ANY_MOD,           Button4, kscrollup,      {.i = 1},		0, /* !alt */ -1 },
 	{ XK_ANY_MOD,           Button5, kscrolldown,    {.i = 1},		0, /* !alt */ -1 },
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
